7feefa185bd145cda30b8127b8513ae42bf3b30f
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-02 15:58:00 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C_sharp_learning
+{
+    class MainClass
+    {
+        static void Main(string[] args) //метод срабатывает при старте программы
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi, everyone\nNew Line");
+            Console.ReadKey(); //параметры не передает. Не закроется пока не введешь значение
+        }
+    }
+}
